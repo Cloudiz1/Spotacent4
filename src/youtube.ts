a704d53e7dd49639dac614b215b3995041c72a0d
Vinny <vincentni2007@gmail.com>
2025-05-24 00:37:18 -0400

    here you go leo :3

--- src/youtube.ts
+++ src/youtube.ts
@@ -15,22 +15,37 @@ function getPlaylists(): Playlist[] {
 async function installSongs() {
     const playlists: Playlist[] = getPlaylists();
 
-    const data = fs.readFileSync(INSTALLED_SONGS_PATH, 'utf-8');
-    const currentlyInstalledTracks = new Set(data.split('\n'));
+    let currentlyInstalledTracks = new Set();
+    if (fs.existsSync(INSTALLED_SONGS_PATH)) {
+        const data = fs.readFileSync(INSTALLED_SONGS_PATH, 'utf-8');
+        currentlyInstalledTracks = new Set(data.split('\n'));
+    }
 
     let newTrackNamesSet: Set<string> = new Set(); 
     for (const playlist of playlists) {
         for (const track of playlist.tracks) {
             const trackName = track.name + " by " + track.artists.join(" and ");
-            const ytQuery = track.name + track.artists.join(" ");
+            const ytQuery = track.name + track.artists[0];
 
             if (!currentlyInstalledTracks.has(trackName)) {
                 const res = await yts(ytQuery);
-                const videos = res.videos.slice(0, 5);
+                const videos = res.videos.slice(0, 3);
+
+                let mostViewed = videos[0];
+                let mostViewedCount: number = videos[0].views;
+
                 videos.forEach((v) => {
-                    console.log( `${ v.views } | ${ v.title } (${ v.timestamp }) | ${ v.author.name }` );
+                    if (v.views > mostViewedCount) {
+                        mostViewedCount = v.views;
+                        mostViewed = v;
+                    }
+                    
+                    // console.log( `${ v.views } | ${ v.title } (${ v.timestamp }) | ${ v.author.name }` );
                 })
+
+                console.log(mostViewed);
             }
+
 
             newTrackNamesSet.add(trackName);
         }
